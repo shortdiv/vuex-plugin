a4f1c09c10737945578e916091fd1e3142e8d9d2
Divya Tagtachian <hello@shortdiv.com>
2020-06-09 06:35:19 -0500

    before after

--- src/plugins/persist-data.js
+++ src/plugins/persist-data.js
@@ -16,9 +16,21 @@ const persistDataPlugin = store => {
       }
     }
   });
-  store.subscribeAction(action => {
-    console.log(action.type);
-    console.log(action.payload);
+  store.subscribeAction({
+    before: (action, state) => {
+      if (action.type === "serviceMachine") {
+        const timeLapsed = new Date().getTime() - state.lastServiced.getTime();
+        console.log(`${timeLapsed / 1000} seconds passed`);
+        console.log("before");
+        console.log(state.lastServiced);
+      }
+    },
+    after: (action, state) => {
+      if (action.type === "serviceMachine") {
+        console.log("after");
+        console.log(state.lastServiced);
+      }
+    }
   });
 };
 
